13b22933210c078cc961e14867d45d097d14c9a0
edwin.segerstedt <edwin.segerstedt@5CD132CZSM.learnet.se>
2023-03-23 15:45:04 +0100

    1:A COMMIT

---
+++ src/buss/Buss.java
@@ -0,0 +1,36 @@
+
+package buss;
+
+import java.util.Scanner;
+
+public class Buss {
+    
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        int persnummer = input.nextInt();
+        System.out.println("ditt personummer " + persnummer);
+        
+        int[] plats = new int[21];
+        
+        for(int i=0;i < plats.length;  i++){
+            if(plats[i] == 0) {
+                plats[i] = persnummer;
+                break;
+            }
+            
+        
+            
+    }
+    }
+        
+        
+        
+        
+        
+        
+        
+     
+    }
+    
+
